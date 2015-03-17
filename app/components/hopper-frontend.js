91f822646aacc0cf960943c20bdd35c98925b6f3
Max Brauer <max@transcode.de>
2015-03-17 15:14:47 +0100

    Renamed action toggleSidenavOpen to toggleIsSidenavOpen.

--- app/components/hopper-frontend.js
+++ app/components/hopper-frontend.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export default Ember.Component.extend({
     isSidenavOpen: false,
     actions: {
-        toggleSidenavOpen: function () {
+        toggleIsSidenavOpen: function () {
             this.set('isSidenavOpen', !this.get('isSidenavOpen'));
         }
     }
