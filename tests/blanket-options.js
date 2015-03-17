9b9e0dc2acebf8800de6ee1c6335d1d7bae22938
Max Brauer <max@transcode.de>
2015-03-17 16:26:26 +0100

    Excluded config and ember initializers from coverage report.

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -1,9 +1,13 @@
 /* globals blanket */
 
 blanket.options({
-   modulePrefix: "hopper-frontend",
-   filter: "//.*hopper-frontend/.*/",
-   antifilter: "//.*(tests|template).*/",
-   loaderExclusions: [],
+    modulePrefix: "hopper-frontend",
+    filter: "//.*hopper-frontend/.*/",
+    antifilter: "//.*(tests|template).*/",
+    loaderExclusions: [
+        'hopper-frontend/config/environment',
+        'hopper-frontend/initializers/app-version',
+        'hopper-frontend/initializers/export-application-global'
+    ],
    enableCoverage: true
 });
