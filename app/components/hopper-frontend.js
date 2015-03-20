4ba740b210aba4e374d6d9eac8dc1c61bb71e925
Max Brauer <max@transcode.de>
2015-03-20 13:27:08 +0100

    Implemented new actions on hopper-frontend.

--- app/components/hopper-frontend.js
+++ app/components/hopper-frontend.js
@@ -2,9 +2,18 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     isElementDrawerOpen: false,
+    isTitleEditing: false,
+    formTitle: 'My awesome Festival',
+
     actions: {
         toggleIsElementDrawerOpen: function () {
             this.set('isElementDrawerOpen', !this.get('isElementDrawerOpen'));
+        },
+        editTitle: function () {
+            this.set('isTitleEditing', true);
+        },
+        acceptTitleChange: function () {
+            this.set('isTitleEditing', false);
         }
     }
 });
