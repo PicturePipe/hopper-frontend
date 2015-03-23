c231fc01919ab74dd9a87275347195ef02fb54e7
Max Brauer <max@transcode.de>
2015-03-23 17:52:25 +0100

    Added element-drawer component for easier adding additonal fields later.
    This component also removes code duplication.

---
+++ app/components/element-drawer.js
@@ -0,0 +1,20 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+    fields: [
+        'Fieldset',
+        'Charfield',
+        'Textfield',
+        'Select',
+        'Multiselect',
+        'Datefield',
+        'Datetimefield',
+        'Filefieled',
+        'Imagefield',
+        'Integerfield',
+        'Mailfield',
+        'Urlfield',
+        'Passwordfield',
+        'Hiddenfield',
+    ]
+});
