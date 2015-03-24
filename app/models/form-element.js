d9c9354c5e5d140d61fea62a8d5a023dede4cd25
Max Brauer <max@transcode.de>
2015-03-24 13:29:41 +0100

    Added fieldType to model of form-element and extended fixture.

--- app/models/form-element.js
+++ app/models/form-element.js
@@ -4,12 +4,13 @@ var FormElement = DS.Model.extend({
     label: DS.attr('string'),
     immutable: DS.attr('bool'),
     required: DS.attr('required'),
+    fieldType: DS.attr('string'),
 });
 
 FormElement.reopenClass({
     FIXTURES: [
-        { id: "1", label: 'First Name', required: false, immutable: true},
-        { id: "2", label: 'Last Name', required: true, immutable: false},
+        { id: "1", label: 'First Name', required: false, immutable: true, fieldType: 'Charfield'},
+        { id: "2", label: 'Last Name', required: true, immutable: false, fieldType: 'Charfield'},
     ]
 });
 
