b81a109d27e0c2362b5f32d9a4a910cb29a5d8e4
Max Brauer <max@transcode.de>
2015-03-24 16:04:07 +0100

    Made new fields automaticaly be in edit mode.

--- app/components/form-element.js
+++ app/components/form-element.js
@@ -1,3 +1,26 @@
 import Ember from 'ember';
 
-export default Ember.Component.extend({});
+export default Ember.Component.extend({
+  _isEditing: false,
+
+  isEditing: function() {
+      return this.get('_isEditing');
+  }.property('_isEditing'),
+
+  didInsertElement: function() {
+    // While inserting a new formElement, it's label will be "" and
+    // we want to edit it. A computet property, looking for _isEditing
+    // and formElement.label would not work, because during render
+    // time of the page the formElement.label would be undefined.
+    if (this.get('formElement.label') === "") {
+      this.set('_isEditing', true);
+    }
+  },
+
+  actions: {
+    togglIsEditing: function() {
+        this.toggleProperty('_isEditing');
+    },
+  }
+
+});
