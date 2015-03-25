36f9c3561c494980af47d4267c058288e3fe460a
Max Brauer <max@transcode.de>
2015-03-25 09:26:05 +0100

    Renamed fieldType to elementType.

--- app/models/form-element.js
+++ app/models/form-element.js
@@ -4,13 +4,13 @@ var FormElement = DS.Model.extend({
     label: DS.attr('string'),
     immutable: DS.attr('bool'),
     required: DS.attr('required'),
-    fieldType: DS.attr('string'),
+    elementType: DS.attr('string'),
 });
 
 FormElement.reopenClass({
     FIXTURES: [
-        { id: "1", label: 'First Name', required: false, immutable: true, fieldType: 'Charfield'},
-        { id: "2", label: 'Last Name', required: true, immutable: false, fieldType: 'Charfield'},
+        { id: "1", label: 'First Name', required: false, immutable: true, elementType: 'Charfield'},
+        { id: "2", label: 'Last Name', required: true, immutable: false, elementType: 'Charfield'},
     ]
 });
 
