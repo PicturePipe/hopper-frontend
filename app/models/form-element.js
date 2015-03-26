2517cc59491c5e1a9a168597b91a8d3a5d2c2cba
Max Brauer <max@transcode.de>
2015-03-26 12:40:41 +0100

    Added name property for form-element

--- app/models/form-element.js
+++ app/models/form-element.js
@@ -1,10 +1,23 @@
 import DS from 'ember-data';
 
+function convertToSlug(text) {
+    return text.toLowerCase().replace(/ /g,'-').replace(/[^\w-]+/g,'');
+}
+
+
 var FormElement = DS.Model.extend({
     label: DS.attr('string'),
     immutable: DS.attr('bool'),
     required: DS.attr('required'),
     elementType: DS.attr('string'),
+
+    name: function() {
+        var name = '';
+        if (typeof(this.get('label')) !== 'undefined') {
+            name = convertToSlug(this.get('label'));
+        }
+        return name;
+    }.property('label')
 });
 
 FormElement.reopenClass({
