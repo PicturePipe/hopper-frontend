fbbe93543c56b075d3c61b4673043267ad33bab7
Max Brauer <max@transcode.de>
2015-04-02 13:46:33 +0200

    Added sorting.

--- app/components/dragable-elements.js
+++ app/components/dragable-elements.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+    didInsertElement: function() {
+        var hopperOutlet = this.$().closest('.hopper-outlet');
+        hopperOutlet.find('.sub-elements').sortable({
+            connectWith: ".sub-elements",
+            cursor: "move",
+            cancel: ".widget-content, .btn",
+            delay: 150,
+            forceHelperSize: true,
+            forcePlaceholderSize: true,
+            toleranceType: "pointer"
+        });
+    }
 });
