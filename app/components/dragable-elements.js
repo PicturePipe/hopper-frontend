a0a1abe9bc844d9ab273de6b2ca367e00173da7e
Max Brauer <max@transcode.de>
2015-04-07 13:13:48 +0200

    dragable-element builds a order obj and sends the updateElementsOrder

--- app/components/dragable-elements.js
+++ app/components/dragable-elements.js
@@ -2,15 +2,40 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
     didInsertElement: function() {
+        var self = this;
         var hopperOutlet = this.$().closest('.hopper-outlet');
         hopperOutlet.find('.sub-elements').sortable({
             connectWith: ".sub-elements",
             cursor: "move",
-            cancel: ".widget-content, .btn",
+            cancel: "input,a",
             delay: 150,
             forceHelperSize: true,
             forcePlaceholderSize: true,
-            toleranceType: "pointer"
+            toleranceType: "pointer",
+            update: function() {
+                var order = {};
+                Ember.$('.field').each(function(index) {
+                    var element = Ember.$(this);
+                    var parentNode = element.parents().eq(3);
+                    if (parentNode.hasClass('hopper-outlet')) {
+                        order[element.data('id')] = {
+                            weight: index,
+                            formElements: {}
+                        };
+                    } else {
+                        order[parentNode.data('id')].formElements[element.data('id')] = {
+                            weight: index,
+                            formElements: {}
+                        };
+                    }
+                });
+                self.sendAction('updateElementsOrder', order);
+            }
         });
+    },
+    actions: {
+        updateElementsOrder: function(order) {
+            this.sendAction('updateElementsOrder', order);
+        }
     }
 });
