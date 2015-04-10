d3812b68d1c283aa6df2fa3849c8a267ab82dfda
Max Brauer <max@transcode.de>
2015-04-10 12:46:01 +0200

    Added test for removeFormElements of dragable-elements component.

--- tests/unit/components/dragable-elements-test.js
+++ tests/unit/components/dragable-elements-test.js
@@ -1,11 +1,32 @@
+import Ember from 'ember';
 import {
   moduleForComponent,
   test
 } from 'ember-qunit';
 
+var fakeFormElements = ['Foo', 'Bar'];
+
+var fakeObject = Ember.Object.extend({
+  formElements: ['Foo', 'Bar'],
+  removeObjects: function(objects) {
+    this.set('formElements', []);
+  },
+});
+
+
+
 moduleForComponent('dragable-elements', {
   // Specify the other units that are required for this test
   // needs: ['component:foo', 'helper:bar']
+});
+
+test('removeFormElements', function(assert) {
+  assert.expect(2);
+  var component = this.subject();
+  var obj = fakeObject.create();
+  assert.equal(obj.get('formElements').length, 2);
+  component.removeFormElements(obj);
+  assert.equal(obj.get('formElements').length, 0);
 });
 
 test('it renders', function(assert) {
