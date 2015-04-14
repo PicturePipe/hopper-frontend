04b3da4845cadfdec471b1c9a9dd05b7c2446cd9
Max Brauer <max@transcode.de>
2015-04-14 14:38:18 +0200

    forms route auto transite to fields of no form is loaded.

--- app/routes/form.js
+++ app/routes/form.js
@@ -2,6 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
     model: function() {
-        return this.store.find('form', 'fixture-0');
+        if (this.store.all('form').get('length')) {
+            return this.store.find('form', 'fixture-0');
+        } else {
+            this.transitionTo('fields');
+        }
     },
 });
