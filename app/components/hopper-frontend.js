ebe02e1c7aa72709764cfde794a1802d33fae1c7
Max Brauer <max@transcode.de>
2015-04-16 16:19:21 +0200

    Added help function for foundation joyride.

--- app/components/hopper-frontend.js
+++ app/components/hopper-frontend.js
@@ -9,6 +9,16 @@ export default Ember.Component.extend({
         // set this static for now
         // will be replaced with our API call later
         this.set('form', this.store.find('form', 1 ));
+        Ember.$(document).foundation({
+                joyride: {
+                    tip_location_patterns    : {
+                        top: ['top'],
+                        bottom: [], // bottom should not need to be repositioned
+                        left: ['right', 'top', 'bottom'],
+                        right: ['left', 'top', 'bottom']
+                    },
+                }
+            });
     },
 
     actions: {
@@ -24,6 +34,13 @@ export default Ember.Component.extend({
         addFormElement: function(field) {
             var newElement = this.store.createRecord('formElement', {label: '', elementType: field});
             this.get('form').get('formElements').addObject(newElement);
+        },
+        startHelp: function() {
+            this.set('isElementDrawerOpen', false);
+            Ember.$(document).foundation('joyride', 'start');
+            if(this.get('currentController').currentRouteName !== 'fields') {
+                this.get('currentController').transitionToRoute('fields');
+            }
         }
     }
 });
