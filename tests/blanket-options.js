599f9a5a3e79f8b61328dcd8b09cd55f70c8a627
Max Brauer <max@transcode.de>
2015-04-20 17:55:52 +0200

    Changed blanket-options to generate lcov.dat

--- tests/blanket-options.js
+++ tests/blanket-options.js
@@ -1,13 +1,27 @@
-/* globals blanket */
+/*globals blanket, module */
 
-blanket.options({
-    modulePrefix: "hopper-frontend",
-    filter: "//.*hopper-frontend/.*/",
-    antifilter: "//.*(tests|template).*/",
-    loaderExclusions: [
-        'hopper-frontend/config/environment',
-        'hopper-frontend/initializers/app-version',
-        'hopper-frontend/initializers/export-application-global'
-    ],
-   enableCoverage: true
-});
+var options = {
+  modulePrefix: "hopper-frontend",
+  filter: "//.*hopper-frontend/.*/",
+  antifilter: "//.*(tests|template).*/",
+  loaderExclusions: [
+    'hopper-frontend/config/environment',
+    'hopper-frontend/initializers/app-version',
+    'hopper-frontend/initializers/export-application-global'
+  ],
+  enableCoverage: true,
+  cliOptions: {
+    jsonOptions: {
+      outputFile: 'test-output.json' // default is 'coverage.json'
+    },
+    lcovOptions: {
+      outputFile: 'lcov.dat'
+    },
+    reporters: ['lcov']
+  }
+};
+if (typeof exports === 'undefined') {
+  blanket.options(options);
+} else {
+  module.exports = options;
+}
