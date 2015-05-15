e18fc9f84200a9cc0ab60f99911e523a7836ea64
Max Brauer <max@transcode.de>
2015-05-15 15:31:54 +0200

    Renamed fields.

--- app/components/element-drawer.js
+++ app/components/element-drawer.js
@@ -1,21 +1,22 @@
 import Ember from 'ember';
 
+
 export default Ember.Component.extend({
     fields: [
-        {title: 'Fieldset', field_name: 'fieldset'},
-        {title: 'Charfield', field_name: 'input'},
-        {title: 'Textfield', field_name: 'textarea'},
-        {title: 'Select', field_name: 'select'},
-        {title: 'Radiobuttons', field_name: 'radio'},
-        {title: 'Checkboxfield', field_name: 'checkbox'},
-        {title: 'Datefield', field_name: 'date'},
-        {title: 'Datetimefield', field_name: 'datetime'},
-        {title: 'Uploadfield', field_name: 'file'},
-        {title: 'Integerfield', field_name: 'integer'},
-        {title: 'Mailfield', field_name: 'mail'},
-        {title: 'Urlfield', field_name: 'url'},
-        {title: 'Passwordfield', field_name: 'password'},
-        {title: 'Hiddenfield', field_name: 'hidden'},
+        {title: 'Field-set', field_name: 'fieldset'},
+        {title: 'One-line Text', field_name: 'input'},
+        {title: 'Multi-line Text', field_name: 'textarea'},
+        {title: 'Selection List', field_name: 'select'},
+        {title: 'Radio Button', field_name: 'radio'},
+        {title: 'Checkbox', field_name: 'checkbox'},
+        {title: 'Date', field_name: 'date'},
+        {title: 'Date & Time', field_name: 'datetime'},
+        {title: 'File Upload', field_name: 'file'},
+        {title: 'Number', field_name: 'integer'},
+        {title: 'Email', field_name: 'mail'},
+        {title: 'URL', field_name: 'url'},
+        {title: 'Password', field_name: 'password'},
+        {title: 'Hidden Field', field_name: 'hidden'},
     ],
 
     actions: {
