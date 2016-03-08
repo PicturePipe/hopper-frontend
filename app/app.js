dab895852326e42ec00b70f1d0747494f740d568
Max Brauer <max@transcode.de>
2016-03-08 11:17:33 +0100

    Added 2 helper functions to the app.
    HOP-79

--- app/app.js
+++ app/app.js
@@ -10,6 +10,17 @@ Ember.MODEL_FACTORY_INJECTIONS = true;
 App = Ember.Application.extend({
   modulePrefix: config.modulePrefix,
   podModulePrefix: config.podModulePrefix,
+  getQueryStrings: function () {
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+      vars[key] = value;
+    });
+    return vars;
+  },
+  getDataUrl: function() {
+    var data_url = this.HOPPER_DATA_URL + this.form_id + '/';
+    return data_url;
+  },
   Resolver
 });
 
